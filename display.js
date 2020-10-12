e2604fa9c3c74094a0f16f1a64fe9d56e92e872e
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-12 17:59:51 +0900

    sửa Hienthi2

--- display.js
+++ display.js
@@ -1,9 +1,9 @@
-function Display() {
+function display() {
     let Receiver = document.getElementById("receiver").value;
     let Date = document.getElementById("date").value;
     let Place = document.getElementById("address").value;
-    let Hienthi1 = Receiver + "thương nhớ,";
-    let Hienthi2 = document.getElementById("ndung").value;
+    let Hienthi1 = Receiver +"" + "thương nhớ,";
+    let Hienthi2 = document.getElementById("ndung").innerText;
     let Hienthi3 = Place + "," + Date;
     document.getElementById("hienthi1").innerHTML = Hienthi1;
     document.getElementById("hienthi2").innerHTML = Hienthi2;
